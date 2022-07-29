d9939d5886436e1d7c84cf36aa4a620fa41604e8
Muhammad Awais <awaisiqbal78692110@gmail.com>
2022-07-29 15:10:51 +0500

    Update App.js

--- App.js
+++ App.js
@@ -1,24 +1,9 @@
 import React from 'react'
-import { View, Text, StyleSheet } from 'react-native'
-import { Colors } from 'react-native/Libraries/NewAppScreen'
+import { View, Text } from 'react-native'
 export default function App() {
   return (
-    <View style={styles.flexContainer}>
-      <Text style={styles.h1}>App</Text>
+    <View>
+      <Text>App</Text>
     </View>
   )
 }
-const styles = StyleSheet.create({
-  flexContainer: {
-    // justifyContent: "center",
-    // alignItems: "center",
-    backgroundColor: "white",
-    flex: 1
-  },
-  h1: {
-    fontSize: 48,
-    fontWeight: "bold",
-    textAlign: "center",
-    color: "black"
-  }
-})
