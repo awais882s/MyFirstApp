093cc9f94ef69ec8bce46dd2da7afd920c37e2db
Muhammad Awais <awaisiqbal78692110@gmail.com>
2022-07-29 11:44:58 +0500

    Update App.js

--- App.js
+++ App.js
@@ -1,10 +1,17 @@
 import React from 'react'
-import { View, Text } from 'react-native'
+import { View, Text, StyleSheet } from 'react-native'
 
 export default function App() {
   return (
-    <View>
+    <View style={styles.flexContainer}>
       <Text>App</Text>
     </View>
   )
 }
+const styles = StyleSheet.create({
+  flexContainer: {
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "red"
+  }
+})
