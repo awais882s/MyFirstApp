17687a3c1f0608e31db2950d9028e5ea66a4b754
Muhammad Awais <awaisiqbal78692110@gmail.com>
2022-07-29 15:14:34 +0500

    update

--- App.js
+++ App.js
@@ -1,9 +1,9 @@
 import React from 'react'
-import { View, Text } from 'react-native'
+import Home from './src/screens/dashboard/Home'
 export default function App() {
   return (
-    <View>
-      <Text>App</Text>
-    </View>
+    <>
+      <Home />
+    </>
   )
 }
