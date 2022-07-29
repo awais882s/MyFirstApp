73316b85df4b63d1172f7633583ca8cd425bd4c1
Muhammad Awais <awaisiqbal78692110@gmail.com>
2022-07-29 12:03:13 +0500

    center

--- App.js
+++ App.js
@@ -1,10 +1,9 @@
 import React from 'react'
 import { View, Text, StyleSheet } from 'react-native'
-
 export default function App() {
   return (
     <View style={styles.flexContainer}>
-      <Text>App</Text>
+      <Text style={styles.h1}>App</Text>
     </View>
   )
 }
@@ -12,6 +11,12 @@ const styles = StyleSheet.create({
   flexContainer: {
     justifyContent: "center",
     alignItems: "center",
-    backgroundColor: "red"
+    backgroundColor: "red",
+    flex: 1
+  },
+  h1: {
+    fontSize: 48,
+    fontWeight: "bold"
+
   }
 })
